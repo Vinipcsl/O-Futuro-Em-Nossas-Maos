ce057c311b6c6dc0f791d09ea812232d85ad3699
richard170 <richardlucas_1300@hotmail.com>
2023-04-20 09:50:00 -0300

    Update pagina.js

--- scripts/pagina.js
+++ scripts/pagina.js
@@ -1,11 +1,1 @@
-document.getElementById("botao1").addEventListener("click", function() {
-	window.location.href = "pagina1.html";
-});
 
-document.getElementById("botao2").addEventListener("click", function() {
-	window.location.href = "pagina2.html";
-});
-
-document.getElementById("botao3").addEventListener("click", function() {
-	window.location.href = "pagina3.html";
-});
